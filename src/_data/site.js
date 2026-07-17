43e60b2650d86f2e43748bfe3b0c3bb7b96a85c0
sudo-rain <8tvckymgjr@privaterelay.appleid.com>
2026-07-17 10:14:46 -0700

    feat: base layout with shared nav and footer

---
+++ src/_data/site.js
@@ -0,0 +1,5 @@
+export default {
+  inviteUrl: "https://discord.com/oauth2/authorize?client_id=1448447132393672807",
+  supportUrl: "https://discord.gg/A56K9EgxMj",
+  inviteLabel: "Add to Discord",
+};
